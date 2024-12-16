0409e21d43886a63c8dbc2a583f079dd15a392a5
Shabix <131671885+Shabi-x@users.noreply.github.com>
2024-12-16 17:38:45 +0800

    feat: add stories test example for Image component (#31)
    * feat(SkillsTag): establish component testing architecture based on storybook lib
    
    * refactor: refactor the structure of kawa-ui&kawa-foundation
    
    * feat: info components
    
    * feat: 初步完成技术栈组件，todo把skilltag的api抽到techstack层
    
    * merge :package.json
    
    * feat(SkillsTag): establish component testing architecture based on storybook lib
    
    * refactor: refactor the structure of kawa-ui&kawa-foundation
    
    * feat: info components
    
    * feat: 初步完成技术栈组件，todo把skilltag的api抽到techstack层
    
    * merge :package.json
    
    * fetch and merge from the upstream
    
    * refactor: 迁移kawa-ui至新架构
    
    * chore: 删除init时的无效stories
    
    * feat: 完成skillList组件，teckstack组件
    
    * feat: add storybook test example for Image component
    
    ---------
    
    Co-authored-by: dundun003 <liangshaojun@papegames.net>

---
+++ packages/components/stories/image.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "../image/Image";
+import type { Meta, StoryFn } from "@storybook/react";
+
+export default {
+  title: "Example/Image",
+  component: Image,
+  argTypes: {
+    onClick: { action: "clicked" },
+  },
+} as Meta;
+
+const Template: StoryFn = (args) => <Image userData={undefined} {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  src: "https://picsum.photos/200/200",
+  alt: "示例图片",
+  width: 200,
+  height: 200,
+  style: { border: "2px solid black" },
+  className: "custom-class",
+  userData: {},
+};
+
+export const WithUserData = Template.bind({});
+WithUserData.args = {
+  src: "xxxxx",
+  alt: "示例图片",
+  width: 200,
+  height: 200,
+  style: { border: "2px solid black" },
+  className: "custom-class",
+  userData: {
+    photoUri: "https://picsum.photos/200/300",
+  },
+};
