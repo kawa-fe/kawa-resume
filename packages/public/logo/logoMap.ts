bb7c66a47b6765eaf367b05f1c5914f06feec1da
dundun003 <liangshaojun@papegames.net>
2024-12-19 15:20:47 +0800

    feat(assets): Add company logo resources for internship section

---
+++ packages/public/logo/logoMap.ts
@@ -0,0 +1,11 @@
+import byteDanceLogo from './byteDance.png';
+import bilibiliLogo from './bilibili.png';
+import kuaiShouLogo from './kuaiShou.png';
+import tencentLogo from './tencent.png';
+
+export const CompanyLogos = {
+  byteDance: byteDanceLogo,
+  bilibili: bilibiliLogo,
+  kuaiShou: kuaiShouLogo,
+  tencent: tencentLogo,
+};
