20ad6ae3e7ade5862b20a7ef7838ab51427fe153
dundun003 <liangshaojun@papegames.net>
2025-01-22 18:09:58 +0800

    chore: tailwindcss配置需要协商

--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./stories/**/*.{js,ts,jsx,tsx}",
     "./**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
