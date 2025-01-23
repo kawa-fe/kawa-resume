77c64f2bdd3ea6f55b2047d42fdaa61828562394
dundun003 <liangshaojun@papegames.net>
2025-01-23 10:00:34 +0800

    feat: 添加list语义化属性

--- packages/components/list/list.tsx
+++ packages/components/list/list.tsx
@@ -6,11 +6,11 @@ const List = memo(
         {String(item)}
       </div> }: ListProps<T>, ref: React.ForwardedRef<HTMLDivElement>) => {
     return (
-      <div ref={ref}>
-        {data.map((item, index) => (
-          <div key={index}>{renderItem(item, index)}</div>
-        ))}
-      </div>
+<div ref={ref} role="list">
+  {data.map((item, index) => (
+    <div key={index} role="listitem">{renderItem(item, index)}</div>
+  ))}
+</div>
     );
   })
 );
